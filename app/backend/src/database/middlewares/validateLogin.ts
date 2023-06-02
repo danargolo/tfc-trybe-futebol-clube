53dfa943bc04922410142b52d30e32b83984c4d4
Danilo Argolo <dan_argolo@hotmail.com>
2023-06-02 00:08:17 -0300

    [fix] Erro lint e class middleware

--- app/backend/src/database/middlewares/validateLogin.ts
+++ app/backend/src/database/middlewares/validateLogin.ts
@@ -1,17 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import HttpException from '../utils/HttpExeception';
 
-
-class  validate{
+class validate {
   public static login = (req: Request, _res: Response, next: NextFunction) => {
-      const { email, password } = req.body;
-  
-      if (!email || !password) {
-        throw new HttpException(400, 'All fields must be filled');
-      }
-      return next();
-  };
+    const { email, password } = req.body;
 
+    if (!email || !password) {
+      throw new HttpException(400, 'All fields must be filled');
+    }
+    return next();
+  };
 }
 
 export default validate;
